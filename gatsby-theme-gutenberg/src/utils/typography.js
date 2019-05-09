79fe39410976d42f8b4d41d9dfa4e568744b4fe1
Wei Gao <wgao19@gmail.com>
2019-05-09 19:41:28 +0800

    Fix dependencies after refactoring

--- gatsby-theme-gutenberg/src/utils/typography.js
+++ gatsby-theme-gutenberg/src/utils/typography.js
@@ -1,4 +1,4 @@
 import Typography from "typography";
-import gutenberg from "typography-theme-gutenberg";
+import gutenberg from "typography-theme-gutenberg-merriweather";
 
 export default new Typography(gutenberg);
