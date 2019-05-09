ed0ac4bce887a77b91b8e8aee70291da4ba5970f
Wei Gao <wgao19@gmail.com>
2019-05-10 02:30:24 +0800

    Remove joining image url

--- gatsby-starter-gutenberg/src/components/SEO/index.jsx
+++ gatsby-starter-gutenberg/src/components/SEO/index.jsx
@@ -24,7 +24,7 @@ class SEO extends Component {
       image = config.siteLogo;
     }
 
-    image = urljoin(config.siteUrl, config.pathPrefix, image);
+    // image = urljoin(config.siteUrl, config.pathPrefix, image);
     const blogURL = urljoin(config.siteUrl, config.pathPrefix);
     const schemaOrgJSONLD = [
       {
