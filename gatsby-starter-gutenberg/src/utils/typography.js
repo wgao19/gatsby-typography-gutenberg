008c1006008a8e30608fa12f1571d8f1f32e6eed
Wei Gao <wgao19@gmail.com>
2019-05-10 23:09:05 +0800

    Remove gatsby-starter-gutenberg from repo

--- gatsby-starter-gutenberg/src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography";
-import gutenberg from "typography-theme-gutenberg";
-import CodePlugin from "typography-plugin-code";
-
-const typography = new Typography(gutenberg);
-typography.plugins = [new CodePlugin()];
-
-export default typography;
